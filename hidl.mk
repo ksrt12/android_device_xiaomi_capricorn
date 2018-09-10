1ba2ef9ebd84e948e5afa92db802c780b57090d3
Vol Zhdanov <wight554@gmail.com>
2018-09-10 18:42:52 +0000

    Switch to power-libperfmgr
    * cleanup unused camera hints
    * revert b296bbf
    
    Change-Id: I6a52a8d0bd527b153dc9558670366b14667803e5
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -95,7 +95,7 @@ PRODUCT_PACKAGES += \
 
 # Power HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service.gemini-libperfmgr
 
 # RenderScript HIDL
 PRODUCT_PACKAGES += \
