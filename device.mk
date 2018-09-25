aca79f8fe3ecfe09bf2a06efd70953f3113fa1f2
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2018-09-25 12:09:23 +0000

    update

--- device.mk
+++ device.mk
@@ -126,13 +126,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libbt-vendor
-
-# Calendar AOSP
-PRODUCT_PACKAGES += \
-    Calendar
 
 # Camera
 PRODUCT_PACKAGES += \
