0c3b687c88701009676526f641a771fdc21af217
Gabor Meszaros <meszaros.gabor.10k@gmail.com>
2018-10-07 19:17:21 +0000

    revert audio to 2.0

--- hidl.mk
+++ hidl.mk
@@ -21,6 +21,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.0-service \
+android.hardware.audio.effect@2.0 \
+	    android.hardware.audio.effect@2.0-impl
+#\
     android.hardware.audio@4.0 \
     android.hardware.audio.common@4.0 \
     android.hardware.audio.common@4.0-util \
