90bca47b294879205c797c569d3142aca55c5d47
DennySPB <dennyspb@gmail.com>
2018-11-27 17:30:48 +0300

    Kang qti performance libs from OP6
    needed for CAF boostframework
    
    Change-Id: I0be80d9419d3a5eb959f2911205e21e7fde39124
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -368,6 +368,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+#QTI performance
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
 # HIDL
 $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 
