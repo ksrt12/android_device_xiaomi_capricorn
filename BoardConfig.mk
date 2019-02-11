c140b1b5629aa7ed17e6950c095e120cd2b81199
Gabor <meszaros.gabor.10k@gmail.com>
2019-02-11 13:42:09 +0100

    update kernel cmdline
    back with lpm_levels.sleep_disabled=1 if this makes deepsleep work I will eat my hat

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 cma=32M@0-0xffffffff
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
